d2f58fbed3d8e80eae6486b1cef8ca841c113370
Andrii Kasiian <kasianandrij@gmail.com>
2024-10-14 15:12:08 +0200

    make build

---
+++ index.b918a95a.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var e=document.querySelector("#largeImg");document.querySelector("#thumbs").addEventListener("click",function(t){var r=t.target.closest("a");if(r&&(t.preventDefault(),r.querySelector("img"))){var n=r.getAttribute("href");e.setAttribute("src",n)}})});
+//# sourceMappingURL=index.b918a95a.js.map
