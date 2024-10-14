71fbd0d14c31f5bc782d4b201a80cc05466a8828
Andrii Kasiian <kasianandrij@gmail.com>
2024-10-14 14:57:31 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const mainImage = document.querySelector('#largeImg');
+
+  const thumbnails = document.querySelector('#thumbs');
+
+  function changeImage(e) {
+    if (e.target.tagName === 'IMG' || e.target.tagName === 'A') {
+      e.preventDefault();
+
+      const necessaryLink = e.target.closest('a');
+      const newSrc = necessaryLink.getAttribute('href');
+
+      mainImage.setAttribute('src', newSrc);
+    }
+  }
+  thumbnails.addEventListener('click', changeImage);
+});
